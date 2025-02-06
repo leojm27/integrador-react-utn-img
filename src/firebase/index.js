ea7975fc6bccc16fa350b819c200c686b1ad5d81
Leonardo J Morales <leom6313@gmail.com>
2025-02-05 23:04:12 -0300

    feat: actualizacion dependencias

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,13 +5,13 @@ import 'firebase/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAoaJ4sTGyHKuxCJXxjWFNrkkzo6MA-aTY",
-    authDomain: "tp-react-utn-2021.firebaseapp.com",
-    projectId: "tp-react-utn-2021",
-    storageBucket: "tp-react-utn-2021.appspot.com",
-    messagingSenderId: "1081071836864",
-    appId: "1:1081071836864:web:3e8901e99a19e6c092d725",
-    measurementId: "G-1ZR5GPV415"
+    apiKey: process.env.API_KEY, //"AIzaSyAoaJ4sTGyHKuxCJXxjWFNrkkzo6MA-aTY",
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
 };
 
 
